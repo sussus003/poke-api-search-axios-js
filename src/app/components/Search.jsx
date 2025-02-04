a785499ec6532cf81dae3799790427df96d199b4
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-05 02:08:46 +0700

    update Display error

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -8,23 +8,30 @@ function Search() {
   const [result, setResult] = useState("");
   const [pokemon, setPokemon] = useState("");
   const [count, setCount] = useState(100);
+  const [error, setError] = useState(true);
 
   const fetchPokemon = async () => {
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
-    setCount(count + 1);
-    setResult(res.data);
-    console.log(count);
-    console.log(result);
+    try {
+      const res = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      );
+      setCount(count + 1);
+      setResult(res.data);
+      console.log(count);
+      console.log(result);
+      setError(false);
+    } catch {
+      setError(true);
+    }
   };
 
   console.log("Pokemon :", pokemon ? pokemon : "Nothing");
 
-  const onKeyDown =(e) => {
-    if(e.key=="Enter"){
+  const onKeyDown = (e) => {
+    if (e.key == "Enter") {
       fetchPokemon();
     }
-  }
+  };
 
   return (
     <div className="container mx-auto my-10 flex justify-center">
@@ -36,7 +43,7 @@ function Search() {
           id="search"
           placeholder="Search Pokemon"
           onChange={(e) => setPokemon(e.target.value)}
-          onKeyDown={(e)=> onKeyDown(e)}
+          onKeyDown={(e) => onKeyDown(e)}
         />
         <input
           className="bg-slate-400 m-2 py-2 px-3 rounded-md hover:cursor-pointer"
@@ -44,11 +51,13 @@ function Search() {
           value="Search"
           onClick={fetchPokemon}
         />
-        
-        {result ? (
-          <DisplayPokemon result={result} />
+
+        {error ? (
+          <h3 className="text-center mt-10 bg-slate-100 p-5 rounded-2xl">
+            Nothing Here! Please try again.
+          </h3>
         ) : (
-          <h3 className="text-center mt-10 bg-slate-100 p-5 rounded-2xl">Nothing Here</h3>
+          <DisplayPokemon result={result} />
         )}
       </div>
     </div>
